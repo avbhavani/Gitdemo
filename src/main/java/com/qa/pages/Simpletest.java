10cb8dc7017b041f4296ce7100276dd4af34bfc7
avsub <gangabhavani.akepati@gmail.com>
2020-03-17 15:53:39 -0500

    newly added command

--- src/main/java/com/qa/pages/Simpletest.java
+++ src/main/java/com/qa/pages/Simpletest.java
@@ -1,5 +1,17 @@
 package com.qa.pages;
 
 public class Simpletest {
-
+	
+	
+	public void testbase() {
+	System.out.println("Simpletest");
+	}
+	
+	
+	public void testset()
+	{
+		System.out.println("testset");
+         }
+	
+	
 }
